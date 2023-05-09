cefdbfc509476f4efe6a1a06889f637a1c0c6cd1
Alex Bolotskov <mamoruuu@proton.me>
2023-05-09 21:05:19 +0600

    refactor: formatting and stuff

--- sitealert.py
+++ sitealert.py
@@ -1,7 +1,7 @@
 import requests
 from requests import Response
 from requests.exceptions import HTTPError
-import sys
+import sys, os
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import configparser
 from selenium import webdriver
@@ -9,55 +9,65 @@ from time import sleep
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+webhook_url = config['Webhook']['url']
+
+screenshotFile = "screenshot.png"
 
 options = webdriver.FirefoxOptions()
 options.headless = True
 driver = webdriver.Firefox(options=options)
 
-def get_http_error_embed(response: Response, error):
-
-	embed = DiscordEmbed(title='HTTP Error occured!', description=f'```{error}```', color='ff0000')
-	embed.add_embed_field(name="Status code", value=f"{response.status_code}", inline=True)
-	embed.add_embed_field(name="Response time", value=f"{response.elapsed}", inline=True)
-	embed.set_timestamp()
-	return embed
-
-def get_exception_embed(error: Exception):
-	embed = DiscordEmbed(title='An error with sitealert occured!', description=error, color='cc9b20')
-	embed.set_timestamp()
-	return embed
-
-def send_to_discord(embed: DiscordEmbed):
-	webhook = DiscordWebhook(url=config['Discord']['webhook_url'])
-
-	driver.get(sys.argv[1])
-	sleep(1)
-	driver.get_screenshot_as_file('screenshot.png')
-	with open("screenshot.png", "rb") as f:
-		webhook.add_file(file=f.read(), filename='screenshot.png')
-	embed.set_image(url='attachment://screenshot.png')
-
-	webhook.add_embed(embed)
-	driver.quit()
-	webhook.execute(remove_files=True)
-
 def main():
 	if len(sys.argv) != 2:
-		raise ValueError('Please provide the URL!')
+		raise ValueError("Please provide the URL to watch!")
 
 	url = sys.argv[1]
-	print(f'Checking the url {url}')
+	print(f"Checking the url {url}...")
+
 	response = requests.head(url)
 
 	try:
 		response.raise_for_status()
 	except HTTPError as http_error:
-		print(f'HTTP error occurred:\n{http_error}')
-		send_to_discord(get_http_error_embed(response, http_error))
-	except Exception as err:
-		print(f'Other exception occurred:\n{err}')
-		send_to_discord(get_exception_embed(err))
+		print(f"An HTTP error occurred:\n{http_error}")
+		send_webhook(url, get_http_error_message_body(response, http_error))
+	except Exception as error:
+		print(f"Other exception occurred:\n{error}")
+		send_webhook(url, get_exception_message_body(error))
 	else:
-		print(f'{url} returns {response.status_code}')
+		print(f"{url} returns {response.status_code}")
+
+def get_http_error_message_body(response: Response, error: HTTPError):
+	# TODO: Form a plain object instead of a whole Discord related class
+	embed = DiscordEmbed(title="HTTP Error occured!", description=f"```{error}```", color="f9e2af")
+	embed.add_embed_field(name="Status code", value=f"{response.status_code}", inline=True)
+	embed.add_embed_field(name="Response time", value=f"{response.elapsed}", inline=True)
+	embed.set_timestamp()
+	return embed
+
+def get_exception_message_body(error: Exception):
+	embed = DiscordEmbed(title="An error with sitealert occured!", description=error, color="f38ba8")
+	embed.set_timestamp()
+	return embed
+
+def remove_screenshot():
+	os.remove(screenshotFile)
+
+def send_webhook(url: str, embed: DiscordEmbed):
+	webhook = DiscordWebhook(url=webhook_url)
+
+	driver.get(url)
+	driver.get_screenshot_as_file(screenshotFile)
+	with open(screenshotFile, "rb") as f:
+		webhook.add_file(file=f.read(), filename=screenshotFile)
+	embed.set_image(url=f"attachment://{screenshotFile}")
+
+	# TODO: Send a simple HTTP request with the 'requests' module
+	webhook.add_embed(embed)
+	webhook.execute(remove_files=True)
+
+	remove_screenshot()
+
+	driver.quit()
 
 main()
